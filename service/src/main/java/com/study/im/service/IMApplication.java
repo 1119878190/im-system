f09592c1fc391ff260974cfd60a5539c36e6683d
lx <1119878190@qq.com>
2023-04-29 16:18:16 +0800

    feat 添加常用pom依赖;添加统一返回对象

---
+++ service/src/main/java/com/study/im/service/IMApplication.java
@@ -0,0 +1,18 @@
+package com.study.im.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author lx
+ * @date 2023/04/29
+ */
+@SpringBootApplication(scanBasePackages = {"com.study.im.service"})
+public class IMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMApplication.class, args);
+    }
+}
